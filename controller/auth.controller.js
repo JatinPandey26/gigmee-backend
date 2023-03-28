c36f44fc6debabd8b64338cb3d8afca28f7d5244
Jatin pandey <jatin26pandey@gmail.com>
2023-03-28 16:39:45 +0530

    gigmee backend

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -63,8 +63,7 @@ export const login = async (req, res, next) => {
     res
       .cookie("accessToken", token, {
         httpOnly: true,
-        sameSite: none,
-        secure: true,
+        
       })
       .status(200)
       .send(info);
