f96b43880efdf01b8a679def8f7bcc2caecd3e03
Jatin pandey <jatin26pandey@gmail.com>
2023-03-28 18:57:03 +0530

    order route

--- routes/order.route.js
+++ routes/order.route.js
@@ -9,7 +9,7 @@ import { verifyToken } from "../middleware/jwt.js";
 const router = express.Router();
 
 // router.post("/:gigId",verifyToken,createOrder);
-router.post("/", verifyToken, getOrders);
+router.get("/", verifyToken, getOrders);
 router.post("/create-payment-intent/:id", verifyToken, createPaymentIntent);
 router.put("/", verifyToken, confirmPayment);
 export { router as orderRouter };
